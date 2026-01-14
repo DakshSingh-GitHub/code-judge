c97604b46b22855a53e3dc2db18bed6edefc1ef0
Daksh Singh <daksh.singh.2407@gmail.com>
2026-01-14 11:52:15 +0530

    Logic addition for User can send only problem ID and problem code, rest all is handled by the API

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
+import json
 from flask import Flask, request, jsonify, render_template
 from runner import run_code_multiple
+import os
 
 app = Flask(__name__)
+
+PROBLEMS_DIR = "problems"
+
 
 @app.route("/")
 def index():
@@ -12,21 +17,53 @@ def index():
 def submit():
     data = request.get_json(force=True)
 
+    problem_id = data.get("problem_id", "")
     code = data.get("code", "")
-    mode = data.get("mode", "")
-    test_cases = data.get("test_cases", [])
 
     if not code: 
         return jsonify({"error": "No code provided"}), 400
     
-    if not mode:
-        return jsonify({"error": "No mode provided"}), 400
+    if not problem_id:
+        return jsonify({"error": "No problem provided"}), 400
 
-    if not test_cases:
-        return jsonify({"error": "No test cases provided"}), 400
+    problem_path = os.path.join(PROBLEMS_DIR, f"{problem_id}.json")
+    if not os.path.exists(problem_path):
+        return jsonify({"error": "Problem not found"})
+    
+    with open(problem_path, "r") as f:
+        problem = json.load(f)
 
-    result = run_code_multiple(code, test_cases)
-    return jsonify(result)
+    judge_mode = problem.get("judge_mode", "ALL")
+
+    test_cases = (
+        problem.get("sample_test_cases", []) +
+        problem.get("custom_test_cases", [])
+    )
+
+    result = run_code_multiple(
+        code = code,
+        test_cases = test_cases,
+        mode = judge_mode
+    )
+
+    visible_results = []
+    for idx, tc_result in enumerate(result["test_case_results"]):
+        if idx < len(problem.get("sample_test_cases", [])):
+            visible_results.append(tc_result)
+        else:
+            visible_results.append({
+                "test_case": tc_result["test_case"],
+                "status": tc_result["status"]
+            })
+    
+    return jsonify({
+        "problem_id": problem_id,
+        "final_status": result["final_status"],
+        "summary": result["summary"],
+        "test_case_results": visible_results
+    })
+            
+
 
 if __name__ == "__main__":
     app.run(debug=True)
