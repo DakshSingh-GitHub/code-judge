2249083922384b3ef81b134ddf929f11b639244b
Daksh Singh <daksh.singh.2407@gmail.com>
2026-01-18 20:52:39 +0530

    Modified the run script

--- script.ps1
+++ script.ps1
@@ -1,4 +1,6 @@
-
-Expand-Archive judge-backend_s.zip ./judge_backend_s
+if (-not (Test-Path -Path "./judge_backend_s" -PathType Container)) {
+    Write-Host "Directory ./judge_backend_s not found. Expanding archive..."
+    Expand-Archive -Path "judge-backend_s.zip" -DestinationPath "./judge_backend_s"
+}
 cd judge_backend_s/judge-backend
 python app.py
